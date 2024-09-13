9f0881e99d850f0ab18a0a43176fbf6c6623a4cc
João Victor Santana Ferreira <joaovictorsantanaf@gmail.com>
2024-09-13 18:12:00 -0300

    add front person

---
+++ src/main/java/com/joaovictor/bank/controller/PersonViewController.java
@@ -0,0 +1,37 @@
+package com.joaovictor.bank.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.joaovictor.bank.dto.PersonDTO;
+import com.joaovictor.bank.entities.Person;
+import com.joaovictor.bank.mapper.PersonMapper;
+import com.joaovictor.bank.service.impl.PersonServiceImpl;
+
+@Controller
+public class PersonViewController {
+
+    @Autowired
+    private PersonServiceImpl personService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<PersonDTO> personsDTO = personService.findAll();
+        model.addAttribute("persons", personsDTO);
+        model.addAttribute("person", new PersonDTO());
+        return "index";
+    }
+
+    @PostMapping("/addPerson")
+    public String addPerson(@ModelAttribute PersonDTO person) {
+        personService.save(person);
+        return "redirect:/";
+    }
+
+}
